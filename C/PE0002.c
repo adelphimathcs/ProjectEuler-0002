20adb1ad685850b47577f30bd4f3979d2a273315
Michael Vessia <michaelvessia@mail.adelphi.edu>
2015-08-06 10:45:29 -0400

    switched if/elif/else to ternary operator
    as suggested by @matthewmatero

--- C/PE0002.c
+++ C/PE0002.c
@@ -17,13 +17,5 @@ int main(int argc, const char *argv[])
 
 
 int fib(int n){
-	if(n==0){
-		return 0;
-	}
-	if(n==1){
-		return 1;
-	}
-	else{
-		return fib(n-1) + fib(n-2);
-	}
+	return n <= 1 ? n : (fib(n-1) + fib(n-2))
 }
